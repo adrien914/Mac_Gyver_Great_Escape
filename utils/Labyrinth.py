7c0b1ff10be80473df91ca89f013ede34b7de9bf
adrien <adriensimon91@gmail.com>
2020-06-26 22:37:50 +0200

    Fully working in command line

--- utils/Labyrinth.py
+++ utils/Labyrinth.py
@@ -1,26 +1,38 @@
+import os
+import random
+
 class Labyrinth:
+    """ This class represents the labyrinth, create a labyrinth by calling Labyrinth(map_name) """
     layout = []
     player_position = []
+    guardian_position = []
+    picked_up_objects = 0
 
     def __init__(self, map):
         with open("maps/" + map) as map_file:
-            for i, line in enumerate(map_file):
-                self.layout.append(list(line.replace("\n", "")))
-        self.seek_player()
-        print(self.player_position)
+            for i, line in enumerate(map_file):  # Get the labyrinth representation from the given file
+                if i < 15:  # if the height goes over 15 don't add the following lines
+                    self.layout.append(list(line[:15].replace("\n", "")))  # Add a line as a list to the layout after removing the line break
+        self.seek_item("P")  # Search for the player's position so we can put it in the player_position variable
+        self.seek_item("E")
+        self.place_items()
+        self.render()  # Render the labyrinth
 
-    def seek_player(self):
-        pos_x = 0
-        pos_y = 0
-        for line in self.layout:
-            for tile in line:
-                if tile == "P":
-                    self.player_position = [pos_x, pos_y]
-                    return
-                else:
-                    pos_x += 1
-            pos_x = 0
-            pos_y += 1
+    def seek_item(self, item):
+        """
+        This function tries to find the player in the actual labyrinth
+        """
+        for index_y, line in enumerate(self.layout):  # Iterate over the y axis
+            for index_x, tile in enumerate(line):  # Iterate over the x axis
+                if tile == item:  # If the tile contains a player
+                    if item == "P":
+                        self.player_position = [index_y, index_x]  # Set the player position to this tile
+                    else:
+                        self.guardian_position = [index_y, index_x]  # Set the guardian position to this tile
+                    return  # stop the function
+        if not self.player_position:
+            print("couldn't find player")
+            exit()
 
     def move(self, direction):
         switch = {
@@ -33,34 +45,77 @@ class Labyrinth:
             "l": self.move_left,
             "left": self.move_left,
         }
-        switch[direction.lower()]()
-        for line in self.layout:
-            print(line)
+        try:
+            switch[direction.lower()]()
+            self.render()
+        except KeyError:
+            print("wrong input")
+            pass
 
     def move_up(self):
-        x, y = self.player_position
-        if self.layout[x][y - 1] != "x":
-            self.layout[x][y] = " "
-            self.layout[x][y - 1] = "P"
-            self.player_position = [x, y - 1]
-
-    def move_right(self):
-        x, y = self.player_position
-        if self.layout[x + 1][y] != "x":
-            self.layout[x][y] = " "
-            self.layout[x + 1][y] = "P"
-            self.player_position = [x + 1, y]
+        y, x = self.player_position
+        if self.check_collision(y - 1, x):
+            self.move_player(y - 1, x)
 
     def move_down(self):
-        x, y = self.player_position
-        if self.layout[x][y + 1] != "x":
-            self.layout[x][y] = " "
-            self.layout[x][y + 1] = "P"
-            self.player_position = [x, y + 1]
+        y, x = self.player_position
+        if self.check_collision(y + 1, x):
+            self.move_player(y + 1, x)
 
     def move_left(self):
-        x, y = self.player_position
-        if self.layout[x - 1][y] != "x":
-            self.layout[x][y] = " "
-            self.layout[x - 1][y] = "P"
-            self.player_position = [x - 1, y]
+        y, x = self.player_position
+        if self.check_collision(y, x - 1):
+            self.move_player(y, x - 1)
+
+    def move_right(self):
+        y, x = self.player_position
+        if self.check_collision(y, x + 1):
+            self.move_player(y, x + 1)
+
+    def move_player(self, new_y, new_x):
+        y, x = self.player_position
+        self.layout[y][x] = " "
+        self.layout[new_y][new_x] = "P"
+        self.player_position = [new_y, new_x]
+        if self.picked_up_objects == 3:
+            y, x = self.guardian_position
+            self.layout[y][x] = " "
+
+    def check_collision(self, y, x):
+        try:
+            tile = self.layout[y][x]
+        except IndexError:
+            return False
+        if tile == "x":
+            return False
+        elif tile == "E":
+            # Lose
+            print("You lose")
+            exit()
+        elif tile == "G":
+            # Win
+            print("You win")
+            exit()
+        elif tile == '0' or tile == '1' or tile == '2':
+            print("picked up:", tile)
+            self.picked_up_objects += 1
+        return True
+
+    def get_free_tiles(self):
+        free_tiles = []
+        for index_y, line in enumerate(self.layout):  # Iterate over the y axis
+            for index_x, tile in enumerate(line):  # Iterate over the x axis
+                if tile == " ":  # If the tile is empty
+                    free_tiles.append([index_y, index_x])
+        return free_tiles
+
+    def place_items(self):
+        free_tiles = self.get_free_tiles()
+        for i in range(0, 3):
+            random_index = random.randint(0, len(free_tiles))
+            x, y = free_tiles.pop(random_index)
+            self.layout[x][y] = str(i)
+
+    def render(self):
+        for line in self.layout:
+            print(line)
