0c128c993ffb7bb49b81ed1b0cb2ccac4909416d
adrien <adriensimon91@gmail.com>
2020-06-26 17:47:50 +0200

    Added player movement and map render in console ( has to be fixed though )

--- Main.py
+++ Main.py
@@ -1,6 +1,15 @@
 from utils.Labyrinth import Labyrinth
+import pygame
+from pygame.locals import KEYUP, KEYDOWN
+
 
 class Main:
 
-    def start_game(self):
+    def __init__(self):
         labyrinth = Labyrinth("sample_map.txt")
+        while True:
+            player_input = input("Direction ou aller: ")
+            labyrinth.move(player_input)
+
+if __name__ == "__main__":
+    Main()
--- utils/Labyrinth.py
+++ utils/Labyrinth.py
@@ -1,7 +1,66 @@
 class Labyrinth:
-    layout = None
+    layout = []
+    player_position = []
 
     def __init__(self, map):
         with open("maps/" + map) as map_file:
-            self.layout = map_file.read()
-        print(self.layout)
+            for i, line in enumerate(map_file):
+                self.layout.append(list(line.replace("\n", "")))
+        self.seek_player()
+        print(self.player_position)
+
+    def seek_player(self):
+        pos_x = 0
+        pos_y = 0
+        for line in self.layout:
+            for tile in line:
+                if tile == "P":
+                    self.player_position = [pos_x, pos_y]
+                    return
+                else:
+                    pos_x += 1
+            pos_x = 0
+            pos_y += 1
+
+    def move(self, direction):
+        switch = {
+            "u": self.move_up,
+            "up": self.move_up,
+            "r": self.move_right,
+            "right": self.move_right,
+            "d": self.move_down,
+            "down": self.move_down,
+            "l": self.move_left,
+            "left": self.move_left,
+        }
+        switch[direction.lower()]()
+        for line in self.layout:
+            print(line)
+
+    def move_up(self):
+        x, y = self.player_position
+        if self.layout[x][y - 1] != "x":
+            self.layout[x][y] = " "
+            self.layout[x][y - 1] = "P"
+            self.player_position = [x, y - 1]
+
+    def move_right(self):
+        x, y = self.player_position
+        if self.layout[x + 1][y] != "x":
+            self.layout[x][y] = " "
+            self.layout[x + 1][y] = "P"
+            self.player_position = [x + 1, y]
+
+    def move_down(self):
+        x, y = self.player_position
+        if self.layout[x][y + 1] != "x":
+            self.layout[x][y] = " "
+            self.layout[x][y + 1] = "P"
+            self.player_position = [x, y + 1]
+
+    def move_left(self):
+        x, y = self.player_position
+        if self.layout[x - 1][y] != "x":
+            self.layout[x][y] = " "
+            self.layout[x - 1][y] = "P"
+            self.player_position = [x - 1, y]
